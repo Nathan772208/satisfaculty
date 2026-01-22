9a1beed197f3753edd185335ef1a9cdd5431e642
Nathan772208 <nathan.brasier@colorado.edu>
2026-01-21 17:05:17 -0700

    Add AES example data, optimization script, and outputs

--- example/optimize.py
+++ example/optimize.py
@@ -9,13 +9,9 @@ Each user can create their own script with different constraint and objective co
 from satisfaculty import *
 
 scheduler = InstructorScheduler(solver_verbose=True)
-scheduler.load_rooms('rooms.csv')
-scheduler.load_courses('courses.csv')
-scheduler.load_time_slots('time_slots.csv')
-
-# Find all 2000-level and 3000-level courses
-courses_2000 = [c for c in scheduler.courses_df['Course'] if c.startswith('DEPT-2')]
-courses_3000 = [c for c in scheduler.courses_df['Course'] if c.startswith('DEPT-3')]
+scheduler.load_rooms('aes-rooms.csv')
+scheduler.load_courses('aes-courses.csv')
+scheduler.load_time_slots('aes-time_slots.csv')
 
 # Add constraints (required for a valid schedule)
 scheduler.add_constraints([
@@ -25,14 +21,14 @@ scheduler.add_constraints([
     RoomCapacity(),
     ForceRooms(),
     ForceTimeSlots(),
-    NoCourseOverlap(courses_2000, name="2000-level"),
-    NoCourseOverlap(courses_3000, name="3000-level"),
 ])
 
 # Define lexicographic optimization objectives (in priority order)
 objectives = [
-    MinimizeClassesAfter('17:00'),
-    MinimizeClassesBefore('9:00'),
+     MinimizeClassesAfter('17:00'),
+    #MinimizeMinutesAfter('17:00'),
+    #MinimizeMinutesAfter('16:00'),
+    #MinimizeClassesBefore('9:00'),
 ]
 
 scheduler.lexicographic_optimize(objectives)
